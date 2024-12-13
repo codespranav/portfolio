eb8a258bc3bdf8c8a9548c88b87ad3868af55e79
Pranav <pranavk24h@gmail.com>
2024-12-13 17:26:27 +0530

    fix api issues

--- src/app/api/blogs/route.js
+++ src/app/api/blogs/route.js
@@ -1,3 +1,3 @@
-export default function GET(req, res) {
-    res.status(200).json({ message: 'Hello from Next.js!' })
-  }
+// export default function GET(req, res) {
+//     res.status(200).json({ message: 'Hello from Next.js!' })
+//   }
